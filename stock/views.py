e396db5b6ff3e49343cb302d866b04bb22e6aae6
hohyunchoi <ghguszxc@gmail.com>
2020-08-10 09:40:24 +0900

    8/10 작업 시작

--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render, redirect
 from stock import models
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 from selenium import webdriver
 import requests as rq
+
 '''
 driver = webdriver.Chrome('C:\ikosmo64\chromedriver.exe')
 driver.get('https://www.ktb.co.kr/trading/popup/itemPop.jspx')
@@ -14,22 +15,19 @@ html = driver.page_source
 soup = bs(html, 'html.parser')
 code = soup.select('.tbody_content > tr > td:first-child')
 name= soup.select('.tbody_content > tr > td:last-child')
-
 codelist= []
 for i in code:
     codelist.append(i.text)
-
 namelist = []
 for i in name:
     namelist.append(i.text.rstrip())
 for code,name in zip(codelist,namelist):
     addcode(code=code,name=name)
-    
+
     create table stock_code(
 c_num number primary key,
 code varchar2(10),
 name varchar2(90));
-
 create sequence stock_code_seq
 increment by 1
 start with 1;
@@ -42,25 +40,28 @@ def login(request):
         return redirect("home")
     else:
         if request.method == 'POST':
-            emailv =request.POST['email']
+            emailv = request.POST['email']
             pwv = request.POST['pw']
-            if models.idcheck(email= emailv,pw=pwv):
-                profile = models.login(email=emailv,pw=pwv)[0]
+            if models.idcheck(email=emailv, pw=pwv):
+                profile = models.login(email=emailv, pw=pwv)[0]
                 request.session['user'] = profile
                 return redirect("home")
             else:
                 msg = "아이디 및 비밀번호를 확인해주세요."
-                return render(request, "loginform.html",{"msg":msg})
+                return render(request, "loginform.html", {"msg": msg})
         else:
             return render(request, "loginform.html")
+
 
 @csrf_exempt
 def join(request):
     return render(request, "loginform.html")
 
+
 def home(request):
     profile = request.session.get("user")
-    return render(request, "stockmain.html",{"profile":profile})
+    return render(request, "stockmain.html", {"profile": profile})
+
 
 def logout(request):
     if 'user' in request.session:
@@ -69,20 +70,23 @@ def logout(request):
     else:
         return redirect("login")
 
+
 def account(request):
     mem_code = request.session.get("user")[0]
     account = models.account(mem_code=mem_code)
-    accountmap = {"ac_num":account[0][0],"ac_balance":account[0][1]}
+    accountmap = {"ac_num": account[0][0], "ac_balance": account[0][1]}
     return render(request, "account.html", {"account": accountmap})
 
+
 def getNaverStockData(**key):
-    url = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok='+key['sosok']+'&page='+str(key["page"])
+    url = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok=' + key['sosok'] + '&page=' + str(key["page"])
     result = pd.read_html(url, encoding="euc-kr")[1]
-    result = result.iloc[:,:-8].dropna().reset_index(drop=True)
+    result = result.iloc[:, :-8].dropna().reset_index(drop=True)
     result['N'] = result['N'].astype(int)
     result['현재가'] = result['현재가'].astype(int)
     result['전일비'] = result['전일비'].astype(int)
     return result
+
 
 def getPaging(**key):
     url = 'https://finance.naver.com/sise/sise_market_sum.nhn'
@@ -92,71 +96,76 @@ def getPaging(**key):
     page = soup.select('.Nnavi')
     return page
 
-def getlastpage(request,**key):
-    url = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok='+key['sosok']+'&page=100'
+
+def getlastpage(request, **key):
+    url = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok=' + key['sosok'] + '&page=100'
     site = rq.get(url)
     site2 = site.content.decode('euc-kr')
     soup = bs(site2, 'html.parser')
     lastpage = soup.select('td.on > a')[0].text
     return int(lastpage)
 
+
 def stockchart(request):
     page = request.GET['page']
     sosok = request.GET['sosok']
-    return render(request,"stockchart.html",{"page":page,"sosok":sosok})
+    return render(request, "stockchart.html", {"page": page, "sosok": sosok})
+
 
 def stockchartajax(request):
     page = request.GET['page']
     sosok = request.GET['sosok']
     print(sosok)
-    stockchart = getNaverStockData(page=page,sosok=sosok).to_html(index=False)
-    return render(request,"stockchartserver.html",{"stockchart":stockchart})
+    stockchart = getNaverStockData(page=page, sosok=sosok).to_html(index=False)
+    return render(request, "stockchartserver.html", {"stockchart": stockchart})
+
 
 def pagingajax(request):
     page = request.GET['page']
     pages = getPaging(page=page)[0]
-    return render(request,"pagingserver.html",{"page":str(pages)})
+    return render(request, "pagingserver.html", {"page": str(pages)})
+
 
 def paging(request):
     page = int(request.GET['page'])
     sosok = request.GET['sosok']
-    startpage= 0
-    endpage=0
-    lastpagev = int(getlastpage(request,sosok=sosok))
-    if (page-5)<1:
+    startpage = 0
+    endpage = 0
+    lastpagev = int(getlastpage(request, sosok=sosok))
+    if (page - 5) < 1:
         startpage = 1
     else:
-        startpage = page -5
-        if startpage > (lastpagev-10):
-            startpage = lastpagev-10
-    if (page+5)>lastpagev:
+        startpage = page - 5
+        if startpage > (lastpagev - 10):
+            startpage = lastpagev - 10
+    if (page + 5) > lastpagev:
         endpage = lastpagev
     else:
-        endpage = page+5
-        if endpage <10:
+        endpage = page + 5
+        if endpage < 10:
             endpage = 10
     pages = '<table><tr>'
     if page != 1:
-        pages += '<td><a href="stockchart?sosok='+sosok+'&page=1"><<</td>'
-        if page <11:
+        pages += '<td><a href="stockchart?sosok=' + sosok + '&page=1"><<</td>'
+        if page < 11:
 
-            pages += '<td><a href="stockchart?sosok='+sosok+'&page=1"><</td>'
+            pages += '<td><a href="stockchart?sosok=' + sosok + '&page=1"><</td>'
         else:
-            pages += '<td><a href="stockchart?sosok='+sosok+'&page='+str(page-10)+'"><</td>'
-    for i in range(startpage,endpage+1):
+            pages += '<td><a href="stockchart?sosok=' + sosok + '&page=' + str(page - 10) + '"><</td>'
+    for i in range(startpage, endpage + 1):
         if i == page:
-            pages += '<td><strong>'+str(i)+'</strong>'
+            pages += '<td><strong>' + str(i) + '</strong>'
         else:
-            pages += '<td><a href="stockchart?sosok='+sosok+'&page='+str(i)+'">'+str(i)+'</a></td>'
+            pages += '<td><a href="stockchart?sosok=' + sosok + '&page=' + str(i) + '">' + str(i) + '</a></td>'
     if page != lastpagev:
 
-        if page > lastpagev -10:
-            pages += '<td><a href="stockchart?sosok='+sosok+'&page='+str(lastpagev)+'">></td>'
+        if page > lastpagev - 10:
+            pages += '<td><a href="stockchart?sosok=' + sosok + '&page=' + str(lastpagev) + '">></td>'
         else:
-            pages += '<td><a href="stockchart?sosok='+sosok+'&page='+str(page+10)+'">></td>'
-        pages += '<td><a href="stockchart?sosok='+sosok+'&page=' + str(lastpagev) + '">>></td>'
+            pages += '<td><a href="stockchart?sosok=' + sosok + '&page=' + str(page + 10) + '">></td>'
+        pages += '<td><a href="stockchart?sosok=' + sosok + '&page=' + str(lastpagev) + '">>></td>'
     pages += '</tr></table>'
-    return render(request,"pagingserver.html",{"page":pages})
+    return render(request, "pagingserver.html", {"page": pages})
 
 
 def stockdetail(request):
@@ -167,18 +176,18 @@ def stockdetail(request):
     """df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
     code = str(df[df['회사명'] == name]['종목코드'].values[0])
     code = code.zfill(6)"""
-    if sosok  == '0':
+    if sosok == '0':
         sosok = 'KOSPI'
     else:
         sosok = 'KOSDAK'
     request.session['sosok'] = sosok
 
-    return render(request,"stockdetail.html",{"name":name,"code":code,'sosok':sosok})
+    return render(request, "stockdetail.html", {"name": name, "code": code, 'sosok': sosok})
 
 
 def today(request):
     code = request.GET['code']
-    url = 'https://finance.naver.com/item/main.nhn?code='+code
+    url = 'https://finance.naver.com/item/main.nhn?code=' + code
     site = rq.get(url)
     site2 = site.content.decode('euc-kr')
     soup = bs(site2, 'html.parser')
@@ -232,14 +241,17 @@ def today(request):
     chart = soup.select('#img_chart_area')[0]['src']
     date = soup.select('.date')[0].text
     hoga = str(soup.select('#tab_con2 > table')[0])
-    hogatable = gethogatable(request,code)
+    hogatable = gethogatable(request, code)
 
-    return render(request,"stocktodayserver.html",{"price":price,"priceupdown":priceupdown,"priceper":priceper,"yesterday":yesterday,"pricemax":pricemax,
-                                                   "dill":dill,"pricesiga":pricesiga,"pricemin":pricemin,"updown":updown,"dillprice":dillprice,
-                                                   "chart":chart,'maxcolor':maxcolor,'mincolor':mincolor,'sigacolor':sigacolor,
-                                                   'color':color,'date':date,"hoga":hogatable})
+    return render(request, "stocktodayserver.html",
+                  {"price": price, "priceupdown": priceupdown, "priceper": priceper, "yesterday": yesterday,
+                   "pricemax": pricemax,
+                   "dill": dill, "pricesiga": pricesiga, "pricemin": pricemin, "updown": updown, "dillprice": dillprice,
+                   "chart": chart, 'maxcolor': maxcolor, 'mincolor': mincolor, 'sigacolor': sigacolor,
+                   'color': color, 'date': date, "hoga": hogatable})
 
-def gethogatable(request,code):
+
+def gethogatable(request, code):
     url = 'https://finance.naver.com/item/main.nhn?code=' + code
     site = rq.get(url)
     site2 = site.content.decode('euc-kr')
@@ -260,7 +272,7 @@ def gethogatable(request,code):
                 ',')))
 
         hogamax.append(hoga2)
-    hogatable = '<table class="hoga"><thead><tr><td>매도잔량</td><td>호가</td><td>매수잔량</td></tr><thead><tfoot><tr><td>'+tdown+'</td><td>잔량합계<br><span>'+dealay+'</span></td><td>'+tup+'</td></tr></tfoot><tbody>'
+    hogatable = '<table class="hoga"><thead><tr><td>매도잔량</td><td>호가</td><td>매수잔량</td></tr><thead><tfoot><tr><td>' + tdown + '</td><td>잔량합계<br><span>' + dealay + '</span></td><td>' + tup + '</td></tr></tfoot><tbody>'
     for i in range(5, 10):
         print(hogamax[i - 5])
         hoga1 = soup.select('#tab_con2 > table .f_down td:nth-child(1)')[i].text
@@ -271,26 +283,30 @@ def gethogatable(request,code):
     for i in range(0, 5):
         hoga1 = soup.select('#tab_con2 > table .f_up td:nth-child(2)')[i].text
         hoga2 = soup.select('#tab_con2 > table .f_up td:nth-child(3)')[i].text
-        hogatable += '<tr class="f_up"><td></td><td>' + str(hoga1) + '</td><td><div class="grp"><div style="width:' + str(
+        hogatable += '<tr class="f_up"><td></td><td>' + str(
+            hoga1) + '</td><td><div class="grp"><div style="width:' + str(
             int((hogamax[i + 5] / max(hogamax)) * 100)) + '%;">' + str(hoga2) + '</div></td></tr>'
     hogatable += '</tbody></table>'
     request.session['hogatable'] = hogatable
     return hogatable
 
+
 def order(request):
     name = request.session['name']
     sosok = request.session['sosok']
     code = request.GET['code']
-    hogatable = gethogatable(request,code)
+    hogatable = gethogatable(request, code)
     soup = bs(hogatable, 'html.parser')
     f_down = soup.select('.f_down td:nth-child(2)')[4].text
     f_down = f_down.replace("\n", "").replace('\t', "")
     f_up = soup.select('.f_up td:nth-child(2)')[0].text
     f_up = f_up.replace("\n", "").replace('\t', "")
 
-    return render(request,"stockorder.html",{"name":name,"code":code,"sosok":sosok,"f_down":f_down,"f_up":f_up})
+    return render(request, "stockorder.html",
+                  {"name": name, "code": code, "sosok": sosok, "f_down": f_down, "f_up": f_up})
+
 
 def hogatable(request):
     code = request.GET['code']
     hogatable = request.session['hogatable']
-    return render(request,"hogatableserver.html",{"hogatable":hogatable})
+    return render(request, "hogatableserver.html", {"hogatable": hogatable})
